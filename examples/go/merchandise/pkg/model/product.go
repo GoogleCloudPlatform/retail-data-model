ad29a509a585827ec210c6a567e3e999e859b582
Ryan McGuinness <rmcguinness@google.com>
2023-09-19 18:30:24 -0400

    Updated inventory and merch info

--- examples/go/merchandise/pkg/model/product.go
+++ examples/go/merchandise/pkg/model/product.go
@@ -8,7 +8,6 @@ import (
 
 type Product merchandise.Product
 type ProductAttribute merchandise.ProductAttribute
-type Variant merchandise.Product_Variant
 type I18nProductHeader merchandise.I18NProductHeader
 
 func NewI18nProductHeader(locale Locale, brand string, name string, shortDescription string, longDescription string) *I18nProductHeader {
@@ -31,31 +30,21 @@ func (n *I18nProductHeader) AddImage(image *Image) {
 	n.Images = append(n.Images, (*common.Image)(image))
 }
 
-func NewProductDetails() *merchandise.Product_Detail {
-	return &merchandise.Product_Detail{
-		Headers:    make([]*merchandise.I18NProductHeader, 0),
-		Attributes: make([]*merchandise.ProductAttribute, 0),
-	}
-}
-
 func NewProduct(header *I18nProductHeader, rating float64) *Product {
 	product := &Product{
-		Id:           (*common.VersionID)(NewVersionID()),
-		BusinessKeys: make([]*common.BusinessKey, 0),
-		Detail:       NewProductDetails(),
-		Rating:       rating,
-		Measurements: make([]*common.NamedMeasure, 0),
-		Variations:   make([]*merchandise.Product_Variant, 0),
-		Suppliers:    make([]*merchandise.Supplier, 0),
+		Id:              (*common.VersionID)(NewVersionID()),
+		BusinessKeys:    make([]*common.BusinessKey, 0),
+		Headers:         make([]*merchandise.I18NProductHeader, 0),
+		Attributes:      make([]*merchandise.ProductAttribute, 0),
+		Rating:          rating,
+		Measurements:    make([]*common.NamedMeasure, 0),
+		Variations:      make([]*common.VersionID, 0),
+		Substitutions:   make([]*common.VersionID, 0),
+		RelatedProducts: make([]*common.VersionID, 0),
+		Suppliers:       make([]*merchandise.Supplier, 0),
 	}
-	product.Detail.Headers = append(product.Detail.Headers, (*merchandise.I18NProductHeader)(header))
+	product.Headers = append(product.Headers, (*merchandise.I18NProductHeader)(header))
 	return product
-}
-
-func NewVariant() *Variant {
-	return &Variant{
-		Details: NewProductDetails(),
-	}
 }
 
 func (p *Product) AddBusinessKey(key string, values ...string) {
@@ -63,17 +52,29 @@ func (p *Product) AddBusinessKey(key string, values ...string) {
 }
 
 func (p *Product) AddHeader(header *I18nProductHeader) {
-	p.Detail.Headers = append(p.Detail.Headers, (*merchandise.I18NProductHeader)(header))
+	p.Headers = append(p.Headers, (*merchandise.I18NProductHeader)(header))
 }
 
 func (p *Product) AddAttribute(attribute *ProductAttribute) {
-	p.Detail.Attributes = append(p.Detail.Attributes, (*merchandise.ProductAttribute)(attribute))
+	p.Attributes = append(p.Attributes, (*merchandise.ProductAttribute)(attribute))
 }
 
 func (p *Product) AddNamedMeasure(measure *NamedMeasure) {
 	p.Measurements = append(p.Measurements, (*common.NamedMeasure)(measure))
 }
 
-func (p *Product) AddVariation(variant *Variant) {
-	p.Variations = append(p.Variations, (*merchandise.Product_Variant)(variant))
+func (p *Product) AddVariation(id *VersionID) {
+	p.Variations = append(p.Variations, (*common.VersionID)(id))
+}
+
+func (p *Product) AddSubstitute(id *VersionID) {
+	p.Substitutions = append(p.Substitutions, (*common.VersionID)(id))
+}
+
+func (p *Product) AddRelatedProduct(id *VersionID) {
+	p.RelatedProducts = append(p.RelatedProducts, (*common.VersionID)(id))
+}
+
+func (p *Product) AddSupplier(supplier *Supplier) {
+	p.Suppliers = append(p.Suppliers, (*merchandise.Supplier)(supplier))
 }
